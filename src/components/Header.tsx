42a61ba20d5bed0bf9c7f1dcae7c1fcf4cfc21b9
Isabella Oliveira <goliveirapk@gmail.com>
2023-07-28 18:34:02 -0300

    style: navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
 export default function ({ title }: { title: string }) {
   return (
-    <header>
-      <h1 className="m-4 font-sans text-4xl">{title}</h1>
-      <nav></nav>
+    <header className="flex justify-between bg-black p-4 text-white">
+      <h1 className="w-fit font-sans text-4xl">{title}</h1>
+      <nav className="flex w-1/3 justify-between text-3xl">
+        <a href="/bugs">bugs</a>
+        <a href="/">home</a>
+        <a href="/report-bug">report bug</a>
+      </nav>
     </header>
   );
 }
