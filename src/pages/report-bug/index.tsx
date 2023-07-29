3eb60bc99b8a095e7ae407cbc764a420d8ca337d
Isabella Oliveira <goliveirapk@gmail.com>
2023-07-29 15:07:56 -0300

    feat: adds report bug form

--- src/pages/report-bug/index.tsx
+++ src/pages/report-bug/index.tsx
@@ -1,8 +1,39 @@
+import { FormEvent } from "react";
 import Header from "~/components/Header";
-export default function () {
+
+interface ILabelInputProps {
+  inputType: string;
+  label: string;
+}
+
+function LabelInput({ inputType, label }: ILabelInputProps) {
   return (
-    <main>
+    <label className="m-1 flex w-1/5 justify-between capitalize">
+      {label}
+      <input type={inputType} className="bg-slate-600 text-white" />
+    </label>
+  );
+}
+
+export default function () {
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    console.log("prevented");
+  }
+
+  return (
+    <main className="h-screen">
       <Header title="report bugs" />
+      <form
+        onSubmit={handleSubmit}
+        className="flex h-full w-screen max-w-full flex-col items-center justify-center"
+      >
+        <LabelInput inputType="text" label="title" />
+        <LabelInput inputType="text" label="description" />
+        <LabelInput inputType="text" label="Assigned Developer" />
+        <button>Report</button>
+      </form>
     </main>
   );
 }
