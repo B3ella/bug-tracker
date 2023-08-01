7e2cc57ebc0747c63a2565372762b97b83038b67
Isabella Oliveira <goliveirapk@gmail.com>
2023-08-01 18:22:05 -0300

    fix: adds custom head for each page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
+import CustomHead from "./CustomHead";
 export default function ({ title }: { title: string }) {
   return (
-    <header className="flex justify-between bg-black p-4 text-white">
-      <h1 className="w-fit font-sans text-4xl">{title}</h1>
-      <nav className="flex w-1/3 justify-between text-3xl">
-        <a href="/bugs">bugs</a>
-        <a href="/">home</a>
-        <a href="/report-bug">report bug</a>
-      </nav>
-    </header>
+    <>
+      <CustomHead title={title} />
+      <header className="flex justify-between bg-black p-4 text-white">
+        <h1 className="w-fit font-sans text-4xl">{title}</h1>
+        <nav className="flex w-1/3 justify-between text-3xl">
+          <a href="/bugs">bugs</a>
+          <a href="/">home</a>
+          <a href="/report-bug">report bug</a>
+        </nav>
+      </header>
+    </>
   );
 }
